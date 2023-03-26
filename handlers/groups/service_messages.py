ed4bbafe8aa63d94da17789bc868ac538d968d30
Abdullajon <101052604+django-dev-uz@users.noreply.github.com>
2023-03-26 19:23:19 +0500

    Upgraded delete message

--- handlers/groups/service_messages.py
+++ handlers/groups/service_messages.py
@@ -8,22 +8,33 @@ from loader import dp
 async def new_member(message: types.Message):
     members = ", ".join([m.get_mention(as_html=True) for m in message.new_chat_members])
     out = await message.reply(f"{members} Muallima Bonu Guruhiga Xush kelibsiz.")
-    await asyncio.sleep(15) 
-    await message.delete()
-    await out.delete()
+    try:
+        await asyncio.sleep(15) 
+        await message.delete()
+        await out.delete()
+    except:
+        print("No message")
 
 
 @dp.message_handler(IsGroup(), content_types=types.ContentType.LEFT_CHAT_MEMBER)
 async def banned_member(message: types.Message):
+
     if message.left_chat_member.id == message.from_user.id:
         out = await message.answer(f"{message.left_chat_member.get_mention(as_html=True)} guruhni tark etdi")
-        await asyncio.sleep(15)
-        await message.delete()
-        await out.delete()
-        return
+        try:
+            await asyncio.sleep(15) 
+            await message.delete()
+            await out.delete()
+            
+        except:
+            print("No message")
+            
     else:
         out = await message.answer(f"{message.left_chat_member.full_name} guruhdan haydaldi "
-                             f"Admin: {message.from_user.get_mention(as_html=True)}.")
-        await asyncio.sleep(15)
-        await message.delete()
-        await out.delete()
+                            f"Admin: {message.from_user.get_mention(as_html=True)}.")
+        try:
+            await asyncio.sleep(15) 
+            await message.delete()
+            await out.delete()
+        except:
+            print("No message")
